bc60c33d05e52fd408acc5115af3ddcbb3a7e330
manel <manelgavalda@iesebre.com>
2016-11-11 16:20:25 +0100

    Repository

---
+++ app/Repositories/Contracts/Repository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Contracts;
+/**
+ * Created by PhpStorm.
+ * User: manel
+ * Date: 11/11/16
+ * Time: 16:16
+ */
+interface Repository
+{
+    public function find($id,$columns = array('*'));
+
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use \App\Repositories\Contracts\Repository;
+
+class UserRepository implements Repository
+{
+
+    public function find($id, $columns = array('*'))
+    {
+        // TODO: Implement find() method.
+    }
+}
