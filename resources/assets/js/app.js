376eeb2e60515d30d19e6c6b97c79d074041801f
manel <manelgavalda@iesebre.com>
2016-11-16 20:00:46 +0100

    Added vue with tasks

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -41,6 +41,18 @@ const app = new Vue({
     methods: {
         reverseMessage: function () {
             this.message=this.message.split('').reverse().join('');
+        },
+        fetchData: function(){
+          //Axios: PEr fer peticions Javascript.
+            var req = new XMLHttpRequest();
+            req.open('GET', 'http://localhost:8001/api/v1/task', false);
+            req.send(null);
+            if (req.status == 200)
+                console.log(req.responseText);
         }
+    },
+    created: function () {
+        console.log('app created');
+        this.fetchData();
     }
 });
