be28e005a43a137d13596d5d81f90cd07cdbad99
manel <manelgavalda@iesebre.com>
2016-11-18 17:29:45 +0100

    todos vue done

--- resources/assets/js/components/todos.vue
+++ resources/assets/js/components/todos.vue
@@ -13,41 +13,35 @@
 </style>
 <script>
     export default {
-        data(){
+        data() {
             return {
-            message: 'Hola que tal',
-            seen=false,
-            todos: [],
-
+                message: 'Hola que tal',
+                seen: false,
+                todos: [],
             }
         },
         created() {
-            console.log('Component todolist created.')
-  //          this.fetchData()
+            console.log('Component todolist created.');
+            this.fetchData();
         },
-        methods:
-        reverseMessage: function () {
-        this.message=this.message.split('').reverse().join('');
-        },
-        fetchData: function() {
-        // GET /someUrl
-        this.$http.get('/api/v1/task').then((response) => {
-        this.todos = response.data.data;
-        }, (response) => {
-        // error callback
-        sweetAlert("Oops...", "Something went wrong!", "error");
-        console.log(response);
-        });
-//TODO: encomptes de ensenyar la llista: fer una taula. El laravel ja te una taula d'exemples afegir simple table(copiar i pegar taulad e dins de pages/table/simple.html(el tenim a node modules, adminlt,pages) i copiar la taula dins la primera table=class i dins el foreach(fiquem els trs de cada tasca(capçalera no). Ficarem name,done,priority.
+        methods: {
+            reverseMessage: function() {
+                this.message = this.message.split('').reverse().join('');
+            },
+            fetchData: function() {
+                // GET /someUrl
+                this.$http.get('/api/v1/task').then((response) => {
+                    console.log(response);
+                    this.todos = response.data.data;
+                }, (response) => {
+                    // error callback
+                    sweetAlert("Oops...", "Something went wrong!", "error");
+                    console.log(response);
+                });
+            }
+        }
     }
+    //TODO: encomptes de ensenyar la llista: fer una taula. El laravel ja te una taula d'exemples afegir simple table(copiar i pegar taulad e dins de pages/table/simple.html(el tenim a node modules, adminlt,pages) i copiar la taula dins la primera table=class i dins el foreach(fiquem els trs de cada tasca(capçalera no). Ficarem name,done,priority.
 </script>
 
 
-<!--data: {-->
-<!--message: 'Hello vue!',-->
-<!--seen:false,-->
-<!--todos: []-->
-<!--},-->
-<!--created: function() {-->
-<!--console.log('Component created!');-->
-<!--}-->
