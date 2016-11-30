ea9138d50a29e4a6a22306df7abd12340a5b0c75
manel <manelgavalda@iesebre.com>
2016-11-30 19:50:33 +0100

    Adding pagination

--- resources/assets/js/components/pagination.vue
+++ resources/assets/js/components/pagination.vue
@@ -1,9 +1,11 @@
 <template>
     <ul class="pagination pagination-sm no-margin pull-right">
         <li><a href="#">&laquo;</a></li>
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
+
+        <li v-for="n in paginationRange" :class="activePage(n)">
+
+        <li><a href="#">{{n}}}</a></li>
+
         <li><a href="#">&raquo;</a></li>
     </ul>
 </template>
@@ -11,5 +13,18 @@
 </style>
 <script>
     export default {
+    computed: {
+        paginationRange () {
+          let start = this.currentPage - this.visiblePages / 2 <= 0
+                        ? 1 : this.currentPage + this.visiblePages / 2 > this.lastPage
+                        ? Util.lowerBound(this.lastPage - this.visiblePages + 1, 1)
+                        : Math.ceil(this.currentPage - this.visiblePages / 2)
+          let range = []
+          for (let i = 0; i < this.visiblePages && i < this.lastPage; i++) {
+            range.push(start + i)
+          }
+          return range
+        }
+      }
     }
 </script>
