59a95c02da4aa78b8addf79a048b54356877163e
manelgavalda <manelgavalda@iesebre.com>
2016-12-02 23:32:38 +0100

    Basic paginator ok TODO:CRUD api

--- resources/assets/js/components/todos.vue
+++ resources/assets/js/components/todos.vue
@@ -1,89 +1,81 @@
+<style>
+</style>
 <template>
     <div>
-        <div class="box">
+        <div class="box box-primary">
             <div class="box-header with-border">
-            <h3 class="box-title">Add task</h3>
+                <h3 class="box-title">Add task</h3>
             </div>
-        <!-- /.box-header -->
-        <!-- form start -->
         <form role="form" action="#">
             <div class="box-body">
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="name" class="form-control" id="name" placeholder="Enter task Name"
+                    <input type="name" class="form-control" id="name" placeholder="Enter task name"
                         v-model="newTodo"
-                        @keyup.enter="addTodo"
-                           >
+                        @keyup.enter="addTodo">
                 </div>
             </div>
-            <!-- /.box-body -->
-
-            <!--<div class="box-footer">-->
-                <!--<button type="submit" class="btn btn-primary">Submit</button>-->
-            <!--</div>-->
         </form>
     </div>
-            <div class="box">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Tasques</h3>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-default">{{visibility}}</button>
-                        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                            <span class="caret"></span>
-                            <span class="sr-only">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#" @click="setVisibility('all')">All</a></li>
-                            <li><a href="#" @click="setVisibility('active')">Active</a></li>
-                            <li><a href="#" @click="setVisibility('completed')">Completed</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="box-body">
-                <table class="table table-bordered">
-                    <thead>
-                    <tr>
-                        <th style="width: 10px">#</th>
-                        <th>Task</th>
-                        <th>Priority</th>
-                        <th>Done</th>
-                        <th>Progress</th>
-                        <th style="width: 40px">Label</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="(todo, index) in filteredTodos">
-                        <td>{{ index + from }}</td>
-                        <td>{{ todo.name }}</td>
-                        <td>{{ todo.priority }}</td>
-                        <td>{{ todo.done }}</td>
-                        <td>
-                            <div class="progress progress-xs">
-                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                            </div>
-                        </td>
-                        <td><span class="badge bg-red">55%</span></td>
-                    </tr>
-                    </tbody>
-
-                </table>
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer clearfix">
-                <span class="pull-left">Showing {{ from }} to {{ to }} {{ total }} entries </span>
-
-                <pagination
-                    :current-page="page"
-                    :items-per-Page="perPage"
-                    :total-items="total"
-                    @page-changed="pageChanged"></pagination><!--TODO api value-->
-                </div>
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Tasques</h3>
+            <div class="btn-group">
+                <button type="button" class="btn btn-default">{{visibility}}</button>
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    <span class="caret"></span>
+                    <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#" @click="setVisibility('all')">All</a></li>
+                    <li><a href="#" @click="setVisibility('active')">Active</a></li>
+                    <li><a href="#" @click="setVisibility('completed')">Completed</a></li>
+                </ul>
             </div>
         </div>
+        <div class="box-body">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th style="width: 10px">#</th>
+                <th>Task</th>
+                <th>Priority</th>
+                <th>Done</th>
+                <th>Progress</th>
+                <th style="width: 40px">Label</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(todo, index) in filteredTodos">
+                <td>{{ index + from }}</td>
+                <td>{{ todo.name }}</td>
+                <td>{{ todo.priority }}</td>
+                <td>{{ todo.done }}</td>
+                <td>
+                    <div class="progress progress-xs">
+                        <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                    </div>
+                </td>
+                <td><span class="badge bg-red">55%</span></td>
+            </tr>
+            </tbody>
+
+        </table>
+    </div>
+    <div class="box-footer clearfix">
+        <span class="pull-left">Showing {{ from }} to {{ to }} of {{ total }} entries </span>
+
+        <pagination
+                :current-page="page"
+                :items-per-page="perPage"
+                :total-items="total"
+                @page-changed="pageChanged"></pagination><!--TODO api value-->
+
+
+    </div>
+    </div>
+        </div>
 </template>
-<style>
-</style>
 <script>
 import Pagination from './pagination.vue'
 
@@ -154,7 +146,6 @@ import Pagination from './pagination.vue'
             this.fetchPage(this.page);
         },
             setVisibility: function(visibility) {
-                console.log("Han fet click");
                 this.visibility=visibility;
             },
             reverseMessage: function() {
@@ -174,7 +165,7 @@ import Pagination from './pagination.vue'
                     sweetAlert("Oops...", "Something went wrong!", "error");
                     console.log(response);
              });
-},
+            },
             fetchPage: function(page) {
             this.$http.get('/api/v1/task?page=' + page).then((response) => {
                     console.log(response);
