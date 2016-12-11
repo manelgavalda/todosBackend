16374314c61369790344562c691fa3cdbd366001
manelgavalda <manelgavalda@iesebre.com>
2016-12-11 20:01:43 +0100

    Added seeder

---
+++ database/seeds/MyUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class UsersTableSeeder.
+ */
+class MyUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            factory(User::class)->create([
+                    "name" => "Manel Gavaldà Andreu",
+                    "email" => "manelgavalda@iesebre.com",
+                    "password" => bcrypt(env('MANEL_PASS', 'secret'))]
+            );
+        } catch (\Illuminate\Database\QueryException $exception) {
+        }
+    }
+}
