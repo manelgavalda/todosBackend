b90a421977b1915791e634839bda55be7d8c5d8e
manel <manelgavalda@iesebre.com>
2016-12-13 20:02:10 +0100

    Gates on Authorization provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,5 +29,42 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        //S'executarà al arrancar el laravel.
+        $this->defineGates();
+    }
+
+    private function defineGates()
+    {
+        Gate::define('impossible-gate',function () {
+            return false; //No autoritzat.
+        });
+
+        Gate::define('easy-gate',function () {
+            return false; //No autoritzat.
+        });
+
+        Gate::define('update-task', function ($user, $task) {
+            return $user->id == $task->user_id;
+        });
+
+        Gate::define('update-task1', function ($user) {
+            return $user->isAdmin();
+        });
+
+        Gate::define('update-task2', function ($user, $task) {
+            if ($user->isAdmin()) return true;
+            return $user->id == $task->user_id;
+        });
+
+        Gate::define('show-tasks', function ($user) {
+            return false;
+        });
+
+        Gate::define('update-task3', function ($user, $task) {
+            if ($user->isAdmin()) return true;
+            if ($user->hasRole('editor')) return true;
+            return $user->id == $task->user_id;
+        });
     }
 }
