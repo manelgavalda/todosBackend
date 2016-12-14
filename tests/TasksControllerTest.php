c551898d82aebd66c34d5ac3993e04fa06d151e0
manelgavalda <manelgavalda@iesebre.com>
2016-12-14 15:22:55 +0100

    Added TasksControllerTest

---
+++ tests/TasksControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Spatie\Permission\Models\Role;
+
+class TasksControllerTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    protected function login()
+    {
+        $user = factory(App\User::class)->create();
+        $this->actingAs($user);
+        return $user;
+    }
+
+    public function testAuthorizedIndex()
+    {
+        $user = $this->login();
+        Role::create(["name" => "admin"]);
+        $user->assignRole('admin');
+
+        $this->get('tasks');
+
+        $this->assertResponseOk();
+    }
+
+    public function testNotAuthorized()
+    {
+        $this->login();
+
+        $this->get('tasks');
+
+        $this->assertResponseStatus(403);
+    }
+}
