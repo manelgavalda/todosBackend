0d885570202851bf55c504833d9597b25190ce35
manelgavalda <manelgavalda@iesebre.com>
2016-12-18 21:53:17 +0100

    Still trying to implement icheck

--- resources/assets/js/components/todo.vue
+++ resources/assets/js/components/todo.vue
@@ -49,18 +49,107 @@
             </td>
 
             <td>
-                <div class="progress progress-xs">
-                    <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                <!-- iCheck -->
+                <div class="box box-success">
+                    <div class="box-header">
+                        <h3 class="box-title">iCheck - Checkbox &amp; Radio Inputs</h3>
+                    </div>
+                    <div class="box-body">
+                        <!-- Minimal style -->
+
+                        <!-- checkbox -->
+                        <div class="form-group">
+                            <label>
+                                <input type="checkbox" class="minimal" checked>
+                            </label>
+                            <label>
+                                <input type="checkbox" class="minimal">
+                            </label>
+                            <label>
+                                <input type="checkbox" class="minimal" disabled>
+                                Minimal skin checkbox
+                            </label>
+                        </div>
+
+                        <!-- radio -->
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="r1" class="minimal" checked>
+                            </label>
+                            <label>
+                                <input type="radio" name="r1" class="minimal">
+                            </label>
+                            <label>
+                                <input type="radio" name="r1" class="minimal" disabled>
+                                Minimal skin radio
+                            </label>
+                        </div>
+
+                        <!-- Minimal red style -->
+
+                        <!-- checkbox -->
+                        <div class="form-group">
+                            <label>
+                                <input type="checkbox" class="minimal-red" checked>
+                            </label>
+                            <label>
+                                <input type="checkbox" class="minimal-red">
+                            </label>
+                            <label>
+                                <input type="checkbox" class="minimal-red" disabled>
+                                Minimal red skin checkbox
+                            </label>
+                        </div>
+
+                        <!-- radio -->
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="r2" class="minimal-red" checked>
+                            </label>
+                            <label>
+                                <input type="radio" name="r2" class="minimal-red">
+                            </label>
+                            <label>
+                                <input type="radio" name="r2" class="minimal-red" disabled>
+                                Minimal red skin radio
+                            </label>
+                        </div>
+
+                        <!-- Minimal red style -->
+
+                        <!-- checkbox -->
+                        <div class="form-group">
+                            <label>
+                                <input type="checkbox" class="flat-red" checked>
+                            </label>
+                            <label>
+                                <input type="checkbox" class="flat-red">
+                            </label>
+                            <label>
+                                <input type="checkbox" class="flat-red" disabled>
+                                Flat green skin checkbox
+                            </label>
+                        </div>
+
+                        <!-- radio -->
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="r3" class="flat-red" checked>
+                            </label>
+                            <label>
+                                <input type="radio" name="r3" class="flat-red">
+                            </label>
+                            <label>
+                                <input type="radio" name="r3" class="flat-red" disabled>
+                                Flat green skin radio
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer">
+                        Many more skins available. <a href="http://fronteed.com/iCheck/">Documentation</a>
+                    </div>
                 </div>
-            </td>
-            <td><span class="badge bg-red">55%</span></td>
-        <td>
-            <span class="btn btn-md btn-danger"  @click="deleteTodo(index,todo.id)">
-                <i class='fa fa-fw fa-trash'/>
-            </span>
-            <span class="btn btn-md btn-info" @click="editTodo(index,todo.id)">
-                <i class='fa fa-fw fa-edit'/>
-            </span>
         </td>
     </tr>
 </template>
