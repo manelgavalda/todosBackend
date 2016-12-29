124c747627171ec803c355c5dd935c724cba97d2
manelgavalda <manelgavalda@iesebre.com>
2016-12-29 13:40:03 +0100

    UserRepository comments

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -6,14 +6,15 @@ use App\Repositories\Contracts\Repository;
 use App\User;
 
 /**
- * Class UserRepository.
+ * Class UserRepository
+ * @package App\Repositories
  */
 class UserRepository implements Repository
 {
+
     /**
      * @param $id
      * @param array $columns
-     *
      * @return mixed
      */
     public function findOrFail($id, $columns = ['*'])
@@ -21,22 +22,43 @@ class UserRepository implements Repository
         return User::findOrFail($id);
     }
 
+    /**
+     * @param int $perPage
+     * @param array $columns
+     * @return mixed
+     */
     public function paginate($perPage = 15, $columns = array('*'))
     {
         return User::paginate($perPage);
     }
 
+    /**
+     * @param array $request
+     *
+     * @return void
+     */
     public function create(array $request)
     {
         User::create($request);
     }
 
-    public function update(array $request,$id)
+    /**
+     * @param array $request
+     * @param $id
+     *
+     * @return void
+     */
+    public function update(array $request, $id)
     {
         $task = $this->findOrFail($id);
         $task->update($request);
     }
 
+    /**
+     * @param $id
+     *
+     * @return void
+     */
     public function delete($id)
     {
         User::findOrFail($id)->delete();
