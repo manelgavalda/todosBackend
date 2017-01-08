9169270d3ff5b55d6900ab9c8825873ff91b2484
manelgavalda <manelgavalda@iesebre.com>
2017-01-08 18:19:37 +0100

    Adding task validator

---
+++ app/Validators/TaskValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Validators;
+
+use Validator;
+
+class TaskValidator
+{
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name'     => 'required|max:255',
+            'email'    => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+            'terms'    => 'required',
+        ]);
+    }
+}
