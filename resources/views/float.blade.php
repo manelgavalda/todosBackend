eab4d542aff87a2a2518597e3cbe846728fab599
manel <manelgavalda@iesebre.com>
2017-01-17 16:20:45 +0100

    Image overflow

--- resources/views/float.blade.php
+++ resources/views/float.blade.php
@@ -23,22 +23,22 @@
         float: left;
         margin-right: 30px;
     }
+    
+    .clearfix {
+        overflow: auto;
+    }
 </style>
 <body>
 
 <h1>Simple float example</h1>
 
-<img src="http://lorempixel.com/200/200/cats" alt="imatge lorem">
+<div class="clearfix">
+    <img src="http://lorempixel.com/200/200/cats" alt="imatge lorem">
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus excepturi itaque non perspiciatis quasi quis
-    voluptate! Aliquam aliquid cum, enim esse itaque laudantium mollitia optio quas sequi, soluta tempore
-    voluptatum!</p>
+    <p class="clearfix">Lorem ipsum </p>
+</div>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto at culpa cumque dicta earum esse
-    exercitationem illum iure, magnam minima officiis perferendis quam quos rerum sequi sint. Amet, porro.</p>
+<h2 class="clearfix">Prova</h2>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur doloremque ducimus est facilis incidunt nam
-    nesciunt nisi odit repellat sequi. Debitis facere illum laboriosam laudantium obcaecati quidem rerum sequi,
-    similique.</p>
 </body>
 </html>
