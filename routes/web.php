e73763f76088693edb90b17b6d6845cdbc0d2f43
manel <manelgavalda@iesebre.com>
2017-01-17 16:22:46 +0100

    Added mvc layoutfloat

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::group(['middleware' => 'auth'], function () {
     });
 
     #adminlte_routes
+    Route::get('layoutfloat', 'LayoutfloatController@index')->name('layoutfloat');
+
     Route::get('boxmodel', 'BoxmodelController@index')->name('boxmodel');
 
     Route::get('/float', function () {
