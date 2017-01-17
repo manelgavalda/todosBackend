c824d601cdaabc404546d309e0c099a797f238fa
manel <manelgavalda@iesebre.com>
2017-01-17 16:10:56 +0100

    spacing

--- resources/views/float.blade.php
+++ resources/views/float.blade.php
@@ -7,11 +7,32 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<style>
+    body {
+        width: 90%;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+    p {
+        line-height: 1.5;
+        word-spacing:2;
+    }
+</style>
 <body>
 
 <h1>Simple float example</h1>
 
-<img src="http://lorempixel.com/200/200" alt="imatge lorem">
+<img src="http://lorempixel.com/200/200/cats" alt="imatge lorem">
 
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus excepturi itaque non perspiciatis quasi quis
+    voluptate! Aliquam aliquid cum, enim esse itaque laudantium mollitia optio quas sequi, soluta tempore
+    voluptatum!</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto at culpa cumque dicta earum esse
+    exercitationem illum iure, magnam minima officiis perferendis quam quos rerum sequi sint. Amet, porro.</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur doloremque ducimus est facilis incidunt nam
+    nesciunt nisi odit repellat sequi. Debitis facere illum laboriosam laudantium obcaecati quidem rerum sequi,
+    similique.</p>
 </body>
 </html>
