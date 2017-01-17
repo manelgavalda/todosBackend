f309ddb11d45709701377e79be1b4adda4bd87b9
manel <manelgavalda@iesebre.com>
2017-01-17 16:33:51 +0100

    html ig tags

--- resources/views/layoutfloat.blade.php
+++ resources/views/layoutfloat.blade.php
@@ -1,21 +1,111 @@
-@extends('adminlte::page')
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
 
-@section('htmlheader_title')
-	Change Title here!
-@endsection
+<style>
+
+	nav{
+		float: left;
+		width:200px;
+		
+	}
+
+	section{
+		margin-left: 50px;
+
+	}
+
+	.clearfix {
+		overflow: auto;
+	}
+
+</style>
+</body>
+
+<div class="clearfix">
+
+	<nav>
+		<ul>
+			<li>Link1</li>
+			<li>Link2</li>
+			<li>Link3</li>
+			<li>Contact</li>
+			<li>About</li>
+		</ul>
+	</nav>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
+
+	<section>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cumque ea est facere harum illo, ipsa, iste iusto magni natus numquam pariatur quas quisquam quod veniam voluptates. Dicta, laborum?
+	</section>
 
 
-@section('main-content')
-	<div class="container-fluid spark-screen">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">Change Header</div>
-					<div class="panel-body">
-						Change content here!
-					</div>
-				</div>
-			</div>
-		</div>
+
+
+
+
+	<div id="footer">
+		@copyright Manel Gavaldà Andreu
 	</div>
-@endsection
+
+</div>
+
+</html>
