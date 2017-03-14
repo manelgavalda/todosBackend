5556c49ee080831ef4ddea4db42b9cba6d7ccc83
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-03-14 17:12:03 +0100

    Hardcoded user post

--- resources/assets/js/components/auth/RegisterForm.vue
+++ resources/assets/js/components/auth/RegisterForm.vue
@@ -1,52 +1,67 @@
 <template>
-
-    <form @submit.prevent="submit">
-        <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Your Name Here" name="name" value=""/>
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
-        </div>
-        <div class="form-group has-feedback">
-            <input type="email" class="form-control" placeholder="Your Email Here" name="email" value=""/>
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-        </div>
-        <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password Here" name="password"/>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        </div>
-        <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password Here" name="password_confirmation"/>
-            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-        </div>
-        <div class="row">
-            <div class="col-xs-1">
-                <label>
-                    <div class="checkbox_register icheck">
-                        <label>
-                            <input type="checkbox" name="terms">
-                        </label>
-                    </div>
-                </label>
-            </div><!-- /.col -->
-            <div class="col-xs-6">
-                <div class="form-group">
-                    <button type="button" class="btn btn-block btn-flat" data-toggle="modal" data-target="#termsModal">Acceptar condicions</button>
-                </div>
-            </div><!-- /.col -->
-            <div class="col-xs-4 col-xs-push-1">
-                <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-            </div><!-- /.col -->
-        </div>
-    </form>
+  <form @submit.prevent="submit">
+    <div class="form-group has-feedback">
+      <input type="text" class="form-control" placeholder="Your Name Here" name="name" value=""/>
+      <span class="glyphicon glyphicon-user form-control-feedback"></span>
+    </div>
+    <div class="form-group has-feedback">
+      <input type="email" class="form-control" placeholder="Your Email Here" name="email" value=""/>
+      <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+    </div>
+    <div class="form-group has-feedback">
+      <input type="password" class="form-control" placeholder="Password Here" name="password"/>
+      <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+    </div>
+    <div class="form-group has-feedback">
+      <input type="password" class="form-control" placeholder="Password Here" name="password_confirmation"/>
+      <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+    </div>
+      <div class="row">
+        <div class="col-xs-1">
+          <label>
+            <div class="checkbox_register icheck">
+              <label>
+                <input type="checkbox" name="terms">
+              </label>
+            </div>
+          </label>
+        </div><!-- /.col -->
+        <div class="col-xs-6">
+          <div class="form-group">
+            <button type="button" class="btn btn-block btn-flat" data-toggle="modal" data-target="#termsModal">Acceptar condicions</button>
+          </div>
+        </div><!-- /.col -->
+        <div class="col-xs-4 col-xs-push-1">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+        </div><!-- /.col -->
+      </div>
+  </form>
 </template>
 <script>
-    export default {
-      mounted() {
-        console.log('Registered Component')
-      },
-      methods: {
-        submit(){
-          console.log('submit')
-        }
+  export default {
+    mounted() {
+      console.log('Registered Component')
+    },
+    methods: {
+      submit(){
+        console.log('submit')
+        //Promise
+        this.$http.post('/register',{
+          name: 'Ramonet',
+          email: 'kragmon@gmail.com',
+          password: '123456',
+          password_confirmation: '123456',
+          terms: true
+        })
+          .then( function (response) {
+            console.log(response)
+          }
+        )
+          .catch( function (error) {
+            console.log(error)
+          }
+        )
       }
     }
+  }
 </script>
